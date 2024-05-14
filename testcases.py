3dfd6123f9108dd4aca0e7068cbb0d6cf564684e
dipGhost <deepghosh@pandatoto.in>
2024-05-14 11:37:49 +0530

    added the negative number testcase

--- testcases.py
+++ testcases.py
@@ -18,6 +18,14 @@ class TestAddFunction(unittest.TestCase):
     def test_custom_delimiter(self):
         self.assertEqual(add("//;\n1;2"), 3)
 
+    def test_negative_numbers_input(self):
+        with self.assertRaises(ValueError) as context:
+            add("-1,2,-3")
+        self.assertEqual(str(context.exception), "negative numbers not allowed -1, -3")
+
+    def test_ignore_numbers_greater_than_1000(self):
+        self.assertEqual(add("2,1001,6"), 8)
+
 
 if __name__ == '__main__':
     unittest.main()
