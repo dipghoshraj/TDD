c4de427ceb89a4f64f81f23152be441b4453ec44
dipGhost <deepghosh@pandatoto.in>
2024-05-14 11:36:25 +0530

    added the new line testcase

--- testcases.py
+++ testcases.py
@@ -12,5 +12,12 @@ class TestAddFunction(unittest.TestCase):
     def test_multiple_numbers_input(self):
         self.assertEqual(add("1,5"), 6)
 
+    def test_newline_between_numbers(self):
+        self.assertEqual(add("1\n2,3"), 6)
+
+    def test_custom_delimiter(self):
+        self.assertEqual(add("//;\n1;2"), 3)
+
+
 if __name__ == '__main__':
     unittest.main()
